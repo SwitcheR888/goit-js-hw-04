d7e9064cd5916e591016a13f57191199dd3c8587
SwitcheR <19fkf86@gmail.com>
2026-09-13 17:57:06 +0300

    task-2=completed

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0;
+    }
+    let totalCalories = 0;
+
+    for (const day of days) {
+        totalCalories += day.calories;
+    }
+    return totalCalories / days.length;
+}
+
+console.log();
+console.log();
+console.log();
