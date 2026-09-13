1040dc8e7bccac7b666d49f3faf8abccbb38635d
SwitcheR <19fkf86@gmail.com>
2026-09-13 20:26:23 +0300

    task-3=completed

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,7 @@
 const profile = {
+    username: 'Jacob',
+    playTime: 300,
+
     changeUsername(newName) {
         this.username = newName;
     },
@@ -6,13 +9,15 @@ const profile = {
         this.playTime += hours;
     },
     getInfo(){
-        return `name ${this.username} time ${this.playTime}`;
+        return `${this.username} has ${this.playTime} active hours!`;
         
     },
 };
 
-console.log(profile.getInfo());
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(profile.changeUsername());
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-console.log(profile.updatePlayTime());
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
